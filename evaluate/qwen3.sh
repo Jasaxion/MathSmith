db4c5f3b0e841c20da9695357d0cfa6fabb5ec8a
Jasaxion <jasaxion@gmail.com>
2025-06-10 20:14:00 +0800

    add full eval script

--- evaluate/qwen3.sh
+++ evaluate/qwen3.sh
@@ -1,9 +1,43 @@
+# AIME 2024
 python infer_longcot.py \
   --data_path data/qwen3/aime2024_test.jsonl \
-  --output_path output/qwen3/aime2024_predictions_thinking.jsonl \
+  --output_path output/qwen3/thinking/aime2024_predictions_thinking.jsonl \
   --model_path 'Qwen/Qwen3-8B' \
   --n_gpus 1 \
   --n 1 \
   --thinking
 
-python cal_acc.py --output_path output/qwen3/aime2024_predictions_thinking.jsonl
+python cal_acc.py --output_path output/qwen3/thinking/aime2024_predictions_thinking.jsonl
+
+# AIME 2025
+python infer_longcot.py \
+  --data_path data/qwen3/aime2025_test.jsonl \
+  --output_path output/qwen3/thinking/aime2025_predictions_thinking.jsonl \
+  --model_path 'Qwen/Qwen3-8B' \
+  --n_gpus 1 \
+  --n 1 \
+  --thinking
+
+python cal_acc.py --output_path output/qwen3/thinking/aime2025_predictions_thinking.jsonl
+
+# MATH500
+python infer_longcot.py \
+  --data_path data/qwen3/math500_test.jsonl \
+  --output_path output/qwen3/thinking/math500_predictions_thinking.jsonl \
+  --model_path 'Qwen/Qwen3-8B' \
+  --n_gpus 1 \
+  --n 1 \
+  --thinking
+
+python cal_acc.py --output_path output/qwen3/thinking/math500_predictions_thinking.jsonl
+
+# Gsm8k
+python infer_longcot.py \
+  --data_path data/qwen3/gsm8k_test.jsonl \
+  --output_path output/qwen3/thinking/gsm8k_predictions_thinking.jsonl \
+  --model_path 'Qwen/Qwen3-8B' \
+  --n_gpus 1 \
+  --n 1 \
+  --thinking
+
+python cal_acc.py --output_path output/qwen3/thinking/gsm8k_predictions_thinking.jsonl
--- evaluate/qwen3_no_thinking.sh
+++ evaluate/qwen3_no_thinking.sh
@@ -1,10 +1,39 @@
-# pip install sympy==1.13
+# AIME 2024
 python infer_longcot.py \
   --data_path data/qwen3/aime2024_test.jsonl \
-  --output_path output/qwen3/aime2024_predictions.jsonl \
+  --output_path output/qwen3/nothinking/aime2024_predictions_thinking.jsonl \
   --model_path 'Qwen/Qwen3-8B' \
   --n_gpus 1 \
   --n 1 \
 
-# pip install sympy==1.10
-python cal_acc.py --output_path output/qwen3/aime2024_predictions.jsonl
+python cal_acc.py --output_path output/qwen3/nothinking/aime2024_predictions_thinking.jsonl
+
+# AIME 2025
+python infer_longcot.py \
+  --data_path data/qwen3/aime2025_test.jsonl \
+  --output_path output/qwen3/nothinking/aime2025_predictions_thinking.jsonl \
+  --model_path 'Qwen/Qwen3-8B' \
+  --n_gpus 1 \
+  --n 1 \
+
+python cal_acc.py --output_path output/qwen3/nothinking/aime2025_predictions_thinking.jsonl
+
+# MATH500
+python infer_longcot.py \
+  --data_path data/qwen3/math500_test.jsonl \
+  --output_path output/qwen3/nothinking/math500_predictions_thinking.jsonl \
+  --model_path 'Qwen/Qwen3-8B' \
+  --n_gpus 1 \
+  --n 1 \
+
+python cal_acc.py --output_path output/qwen3/nothinking/math500_predictions_thinking.jsonl
+
+# Gsm8k
+python infer_longcot.py \
+  --data_path data/qwen3/gsm8k_test.jsonl \
+  --output_path output/qwen3/nothinking/gsm8k_predictions_thinking.jsonl \
+  --model_path 'Qwen/Qwen3-8B' \
+  --n_gpus 1 \
+  --n 1 \
+
+python cal_acc.py --output_path output/qwen3/nothinking/gsm8k_predictions_thinking.jsonl
